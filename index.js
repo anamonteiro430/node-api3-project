4be9717b35ab5039e9c111c7da4f3b55f4992991
kika <anamonteiro430@gmail.com>
2020-02-14 00:10:13 +0000

    deploying

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const server = require('./server.js');
 
-server.listen(5000, () => {
-	console.log('\n*** Server running on http://localhost:5000 ***\n');
+const port = 5000;
+server.listen(port, () => {
+	console.log(`\n*** Server running on http://localhost:${port} ***\n`);
 });
